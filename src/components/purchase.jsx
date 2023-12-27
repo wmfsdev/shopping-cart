dd3d63e22ed3e116babc5258a1c875d3873a21e0
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-12-27 21:16:32 +0000

    add handleClick to deal with conditionals for adding to or updating cart state

--- src/components/purchase.jsx
+++ src/components/purchase.jsx
@@ -4,25 +4,34 @@ const Purchase = ({title, img, price, id}) => {
 
     const [cartItems, setCartItems] = useOutletContext()
 
-    function addToCart(e) {
+    function handleClick(e) {
         e.preventDefault()
+
+        if (cartItems.some(obj => obj.id === id)) {
+            updateCart(e)
+        } else addToCart(e)
+    }
+    
+    function addToCart(e) {
         const data = new FormData(e.target)
         const quantity = data.get("quantity");
-      
+    
         const item = {
             title: title,
             img: img,
             price: price
         }
-      
-       
         setCartItems([...cartItems, {item: item, id: id, quantity: Number(quantity)}])
         console.log(cartItems)
+    }
+
+    function updateCart(e) {
+        console.log("updating")
     }
     
     return (
         <>
-        <form action="" onSubmit={addToCart}>
+        <form action="" onSubmit={handleClick}>
         <input type="number" id="quantity" name="quantity" placeholder="0"/>
         {/* <button type="submit" onClick={addToCart}>ADD</button> */}
         <button type="submit">ADD</button>
